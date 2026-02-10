b3e97b75b4281ddb26d39622d0d639ba406c1838
Marion Prat <marion.prat@bristol.ac.uk>
2026-02-10 17:30:41 +0000

    adding schumpeterian model

---
+++ 04-Schumpeterian-model.R
@@ -0,0 +1,17 @@
+# The Schumpeterian growth model {#SM}
+
+-------------------------------------------------------------------------------------
+
+  By the end of this chapter, you should be able to:
+
+- Describe the toy version of the Schumpeterian model developed by Aghion and Howitt (2005).
+- Understand how the model incorporate key features of endogenous growth theories and Schumpeter's insights.
+- Undertand how the research arbitrage condition works.
+- Analyse the effect of the
+
+
+-------------------------------------------------------------------------------------
+
+  ## Introduction
+
+
